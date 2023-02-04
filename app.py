b782058626a3f65e214276edd6f403c0e44a9d04
thimxokeit <shimsky95@gmail.com>
2023-02-04 16:28:26 +0900

    _selected 추가

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import requests
 from flask import Flask, render_template, request, jsonify
 
-
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -10,39 +9,33 @@ import certifi
 ca = certifi.where()
 client = MongoClient(
     'mongodb+srv://spartsyl:U6V10Jk7juI7SMEX@Cluster0.fsdg43e.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.Bibimbap
+db = client.Bibimbapx
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 cid = 'aa4b6b300fdb4797bfe7625e121c99cd'
 secret = 'ddbca51759f340e58471ec1b929bad7b'
+
+
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-from datetime import datetime
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route("/playlist", methods=["GET"])
-def playlist_get():
-    playlist_list = list(db.playlist.find({}, {'_id': False}))
-    return jsonify({'playlist': playlist_list})
-
-
 @app.route("/playlist", methods=["POST"])
-def spotify_search():
-
-
+def playlist_post():
+    track_input_receive = request.form.get('track_input')
 
     track_results = []
     artists_results = []
-    image_results = []
 
-    track_search = sp.search(track_input_receive, limit=10, type='track', market=None)
+    track_search = sp.search(q=track_input_receive, limit=10, type='track', market=None)
 
     for track in track_search['tracks']['items']:
 
@@ -50,50 +43,32 @@ def spotify_search():
             artists_result = artist['name']
             artists_results.append(artists_result)
 
-        for image in track['images']:
-            image_result = image[0]['url']
-            image_results.append(image_result)
-
         track_result = track['name']
         track_results.append(track_result)
 
-
-
-
-def search_track():
-    track_input_receive = request.form.get('track_input')
-
-    now = datetime.now()
-    date_input = now.date()
-    hour_input = now.hour
-    count = db.playlist.count_documents({"track": track_input_receive, "hour": hour_input})
-
-    if count > 0 :
-        db.playlist.update_one({"track": track_input_receive, "hour": hour_input}, {"$inc": {"count": 1}})
-
-    else:
-        db.playlist.insert_one({"track": track_input_receive, "date": date_input, "hour": hour_input, "count": 1})
-
-
-
-def playlist_post():
+def selected_title():
 
 
     date_input_receive = request.form['date_input']
     hour_input_receive = request.form['hour_input']
-    likes = request.form['like']
+    count = db.playlist.count()
+
 
     doc = {
-        'track': track_results,
-        'artists': artists_results,
-        'image': image_results,
+        'track': track_selected,
+        'artists': artists_selected,
         'date': date_input_receive,
         'hour': hour_input_receive,
-        'likes': likes,
         'count': count,
 
     }
     db.playlist.insert_one(doc)
+
+
+@app.route("/playlist", methods=["GET"])
+def playlist_get():
+    playlist_list = list(db.playlist.find({}, {'_id': False}))
+    return jsonify({'playlist': playlist_list})
 
 
 if __name__ == '__main__':
