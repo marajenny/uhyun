9654790e10c6a7d3cb9bb8851f41b0eac9e03d72
thimxokeit <shimsky95@gmail.com>
2023-02-04 16:48:12 +0900

    album image갖고오기

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import certifi
 ca = certifi.where()
 client = MongoClient(
     'mongodb+srv://spartsyl:U6V10Jk7juI7SMEX@Cluster0.fsdg43e.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.Bibimbapx
+db = client.Bibimbap
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -28,14 +28,15 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/playlist", methods=["POST"])
-def playlist_post():
+@app.route("/search_results", methods=["POST"])
+def search_results():
     track_input_receive = request.form.get('track_input')
+
+    track_search = sp.search("attention", limit=10, type='track', market=None)
 
     track_results = []
     artists_results = []
-
-    track_search = sp.search(q=track_input_receive, limit=10, type='track', market=None)
+    image_results = []
 
     for track in track_search['tracks']['items']:
 
@@ -43,9 +44,32 @@ def playlist_post():
             artists_result = artist['name']
             artists_results.append(artists_result)
 
+        album = track['album']
+        for image in album['images']:
+            image_result = image['url']
+            image_results.append(image_result)
+
         track_result = track['name']
         track_results.append(track_result)
 
+    print(track_results[0])
+    print(artists_results[0])
+    print(image_results[0])
+
+    print(track_results[1])
+    print(artists_results[1])
+    print(image_results[1])
+
+    doc = {
+        'track': track_results,
+        'artists': artists_results,
+        'image' : image_results,
+
+    }
+    db.search_results.insert(doc)
+
+
+@app.route("/playlists", methods=["POST"])
 def selected_title():
 
 
