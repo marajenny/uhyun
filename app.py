1be5caeebd015759d89b28e1e041999da633009e
thimxokeit <shimsky95@gmail.com>
2023-02-05 11:30:54 +0900

    url 크롤링 추가

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 import requests
 from flask import Flask, render_template, request, jsonify
 
@@ -7,19 +10,16 @@ from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
+
 client = MongoClient(
     'mongodb+srv://spartsyl:U6V10Jk7juI7SMEX@Cluster0.fsdg43e.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.Bibimbap
 
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
 cid = 'aa4b6b300fdb4797bfe7625e121c99cd'
 secret = 'ddbca51759f340e58471ec1b929bad7b'
-
-
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 
 
@@ -32,38 +32,30 @@ def home():
 def search_results():
     track_input_receive = request.form.get('track_input')
 
-    track_search = sp.search("attention", limit=10, type='track', market=None)
-
-    track_results = []
-    artists_results = []
-    image_results = []
+    track_search = sp.search(track_input_receive, limit=10, type='track', market=None)
 
     for track in track_search['tracks']['items']:
-
         for artist in track['artists']:
             artists_result = artist['name']
             artists_results.append(artists_result)
 
-        album = track['album']
-        for image in album['images']:
-            image_result = image['url']
-            image_results.append(image_result)
+            album = track['album']
+            for image in album['images']:
+                if image['height'] == 640 and image['width'] == 640:
+                    image_result = image['url']
+                    image_results.append(image_result)
+
+            url_result = album['external_urls']['spotify']
+            url_results.append(url_result)
 
         track_result = track['name']
         track_results.append(track_result)
 
-    print(track_results[0])
-    print(artists_results[0])
-    print(image_results[0])
-
-    print(track_results[1])
-    print(artists_results[1])
-    print(image_results[1])
-
     doc = {
         'track': track_results,
         'artists': artists_results,
-        'image' : image_results,
+        'image': image_results,
+        'url' : url_results
 
     }
     db.search_results.insert(doc)
